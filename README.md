6f4ce848e203208302999661707d465f2ad47cfe
Murilo Portugal <murilo.portugal@gmail.com>
2020-05-09 19:55:48 -0300

    Finalizado desafio 4

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ Repositório com os módulos e desafios do GoStack BootCamp 11.
 :ballot_box_with_check: Nível 1 - [Desafio Conceitos do Node.js](https://github.com/muriloportugal/bootcamp11-desafio2-conceitos-node.js)  
 :ballot_box_with_check: Nível 1 - [Front-end com ReactJS](./frontend)  
 :ballot_box_with_check: Nível 1 - [Desafio Conceitos do ReactJS](https://github.com/muriloportugal/bootcamp11-desafio3-conceitos-reactJS)  
-:arrow_forward: Nível 1 - Mobile com React Native 
-
+:ballot_box_with_check: Nível 1 - [Mobile com React Native](https://github.com/muriloportugal/bootcamp11-desafio4-conceitos-react-native)  
+:arrow_forward: TypeScript  
 ___
 
 ## :memo: Licença
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,87 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 
+import api from './services/api';
+
+//SafeAreaView utilza somente o espaço visível da aplicação, não vai pra
+//baixo da statusbar.
+//StatusBar é a barra onde mostra o relógio, sinal wifi e 3g, bateria, etc
 export default function App() {
-  return <View />;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Murilo Portugal',
+    });
+
+    setProjects([...projects, response.data]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar Projetos</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      {/* <View style={styles.container}>
+        {projects.map(project => (
+          <Text style={styles.project} key={project.id}>
+            {project.title}
+          </Text>
+        ))}
+      </View> */}
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  //Podemos criar quantas propriedade quisermos, e o nome delas (container) é
+  //irrelevante
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+  project: {
+    color: '#fff',
+    fontSize: 30,
+  },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
